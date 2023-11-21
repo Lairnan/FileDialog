9a88d5fa00cb8baa6377eccb18cf61cb17244eb4
Alexey <alexey.saulin03@mail.ru>
2023-11-21 23:38:08 +0300

    Added main converters

---
+++ OpenDialogLibrary/BehaviorsFiles/FMainCollectionStatic.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+using OpenDialogLibrary.Models.Implementation;
+using OpenDialogLibrary.Models.Interfaces;
+
+namespace OpenDialogLibrary.BehaviorsFiles;
+
+public static class FMainCollectionStatic
+{
+    public static async void AddFMains(this ObservableCollection<FMain> collection, string path)
+    {
+        await foreach (var folder in GetFolders(path))
+            collection.Add(folder);
+
+        await foreach (var file in GetFiles(path))
+            collection.Add(file);
+
+    }
+
+    private static async IAsyncEnumerable<Folder> GetFolders(string path)
+    {
+        var dirs = System.IO.Directory.GetDirectories(path);
+        foreach (var dir in dirs)
+            yield return await Task.Run(() => new Folder(dir));
+    }
+
+    private static async IAsyncEnumerable<File> GetFiles(string path)
+    {
+        var files = System.IO.Directory.GetFiles(path);
+        foreach (var file in files)
+            yield return await Task.Run(() => new File(file));
+    }
+}
---
+++ OpenDialogLibrary/BehaviorsFiles/IconConverter.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Windows.Media.Imaging;
+
+namespace OpenDialogLibrary.BehaviorsFiles;
+
+internal static class IconConverter
+{
+    internal static async Task<byte[]> GetFileIcon(string path)
+    {
+        try
+        {
+            var fileIcon = Icon.ExtractAssociatedIcon(path);
+            return fileIcon == null
+                ? await GetDefaultImage()
+                : await IconToBytes(fileIcon);
+        }
+        catch (Exception ex)
+        {
+            Debug.WriteLine($"Ошибка: {ex.Message}");
+            return await GetDefaultImage();
+        }
+    }
+
+    private static async Task<byte[]> IconToBytes(Icon icon)
+    {
+        using var ms = new MemoryStream();
+
+        try { icon.Save(ms); }
+        finally { ms.Close(); await ms.DisposeAsync(); }
+        return ms.ToArray();
+    }
+
+    private static async Task<byte[]> GetDefaultImage()
+    {
+        var data = await GetFromResource("/Images/404.jpg");
+        return data;
+    }
+
+    public static async Task<byte[]> GetFromPathUri(string path)
+    {
+        var uri = new Uri(path);
+        var bitmapImage = new BitmapImage(uri);
+
+        var encoder = new JpegBitmapEncoder();
+        encoder.Frames.Add(BitmapFrame.Create(bitmapImage));
+        using var ms = new MemoryStream();
+        encoder.Save(ms);
+        var data = ms.ToArray();
+        await ms.DisposeAsync();
+        
+        return data;
+    }
+
+    public static async Task<byte[]> GetFromResource(string path)
+    {
+        path = "pack://application:,,,/OpenDialogLibrary;component" + path;
+        return await GetFromPathUri(path);
+    }
+}
